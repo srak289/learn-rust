2671d2eb6e7cb7c82498aa52c9af9dcf72b84d99
Spencer Rak <spencer.rak@hadronindustries.com>
2025-12-25 13:34:41 -0500

    Snapshot

--- 11_combinations/src/main.rs
+++ 11_combinations/src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
+fn permute(buf: String) {
+    let mut b = Vec::from(buf);
+    println!("{:?}", b);
+    let mut res = Vec<String>::new();
+    fn rpermute(v: Vec, n: u8) {
+    }
+    rpermute(b, 0);
+    for a in res {
+        println!("{a}");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter string:");
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf);
+    buf = buf.trim_end().into();
+    permute(buf);
 }
